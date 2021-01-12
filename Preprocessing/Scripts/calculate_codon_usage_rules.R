3ba3a6917fb415061112712c6b1dff8fdb3a3543
astrasb <astra.bryant@gmail.com>
2021-01-12 12:44:00 -0800

    Update adding calculation of relative adaptiveness to preprocessing stage. Also updating app name to Wild Worm Codon Adapter

--- Preprocessing/Scripts/calculate_codon_usage_rules.R
+++ Preprocessing/Scripts/calculate_codon_usage_rules.R
@@ -1,11 +1,24 @@
-# This script takes species-specific counts of codon occurances and calculates the frequency each codon "i" encodes amino acid "AA"
-# These values will be passed to the quantification of relative adaptiveness, the first step for calculating codon adaptation index.
+# This script generates lookup tables for optimal codons in different
+# worm species, including:
+# *Strongyloides spp.*  
+# *C. elegans*
+# *Pristionchus pacificus*
+# *Brugia spp*
 
-# Generate tibble with stop codon codes
-stop_cdns <- tibble(AA = factor("*","*","*"),
-                    Codon = c("TAA", "TAG", "TGA"),
-                    Frequency = c(0,0,0))
+# First, species-specific counts of codon occurances are used to calculate 
+# the frequency each codon "i" encodes amino acid "AA"
+# These values are passed to the quantification of relative adaptiveness, 
+# the first step for calculating codon adaptation index.
+# These relative adaptiveness values are filtered to find the codon with the 
+# highest value per amino acid, and those "optimal" codons are saved in a .csv
+# file for use in the Wild Worm Codon Adaptor App
 
+library(tidyverse)
+library(magrittr)
+library(seqinr)
+
+
+# Strongyloides ----
 # Load S. ratti count data
 # Source: Mitreva et al 2006; counts taken from 50 most common expressed sequence tag clusters (putative genes)
 Sr.dat <- read_csv('../Data/Sr_top50_usage_counts.csv',
@@ -20,10 +33,10 @@ Sr.codon.freq <- Sr.dat %>%
     dplyr::mutate (Frequency = Count / sum(Count)) %>%
     dplyr::mutate (Frequency = Frequency *100) %>%
     dplyr::mutate (Frequency = signif(Frequency, digits = 9)) %>%
-    dplyr::full_join(stop_cdns, by = c("AA", "Codon", "Frequency")) %>%
     dplyr::rename("Sr_optimal" = "Frequency") %>%
     dplyr::select(!Count)
 
+# C. elegans ----
 # Load C. elegans count data
 # Soruce: Sharp and Bradnam, 1997; https://www.ncbi.nlm.nih.gov/books/NBK20194/
 Ce.dat <- read_csv('../Data/Ce_usage_counts.csv', 
@@ -39,40 +52,109 @@ Ce.codon.freq <- Ce.dat %>%
     dplyr::mutate (Frequency = Count / sum(Count)) %>%
     dplyr::mutate (Frequency = Frequency *100) %>%
     dplyr::mutate (Frequency = signif(Frequency, digits = 9)) %>%
-    dplyr::full_join(stop_cdns, by = c("AA", "Codon", "Frequency")) %>%
     dplyr::rename("Ce_optimal" = "Frequency") %>%
     dplyr::select(!Count)
 
-codon_usage_chart <- dplyr::full_join(Sr.codon.freq, 
-                                      Ce.codon.freq,
-                                      by = c("AA", "Codon")
+# B. malayi ----
+# Load B. malayi count data
+# Source: http://big.icp.ucl.ac.be/~opperd/private/C_U_B_%20Table.html
+Bm.dat <- read_csv('../Data/Bm_optimal_usage_counts.csv', 
+                   quote = "", 
+                   col_types = 'ccd'
 )
 
-write_csv(codon_usage_chart,
-          path = "../Outputs/codon_usage_chart.csv")
+Bm.codon.freq <- Bm.dat %>%
+    dplyr::mutate(AA = seqinr::a(AA)) %>%
+    dplyr::arrange(AA, Codon) %>%
+    dplyr::mutate(AA = factor(AA)) %>%
+    group_by(AA) %>%
+    dplyr::mutate (Frequency = Count / sum(Count)) %>%
+    dplyr::mutate (Frequency = Frequency *100) %>%
+    dplyr::mutate (Frequency = signif(Frequency, digits = 9)) %>%
+    dplyr::rename("Bm_optimal" = "Frequency") %>%
+    dplyr::select(!Count)
 
-## Calculate the relative adaptiveness of each codon for Strongyloides and
-## C. elegans usage rules. Note: this computation is done by the Strongyloides
+
+# Merge species ----
+codon_usage_chart <- dplyr::full_join(Sr.codon.freq, 
+                                      Ce.codon.freq,
+                                      by = c("AA", "Codon") 
+)%>%
+    dplyr::full_join(Bm.codon.freq,
+                     by = c("AA", "Codon"))
+
+# Generate Relative Adaptiveness Charts for each species ----
+## Calculate the relative adaptiveness of each codon for each species. 
+## Note: this computation is done by the Strongyloides
 ## Codon Adapter App - it is replicated here so users can easily access the 
-## reltaive adaptiveness values.
-Sr_codonChart <- codon_usage_chart %>%
-    dplyr::select(-Ce_optimal) %>%
+## relative adaptiveness values.
+Sr_codonChart <- Sr.codon.freq %>%
+    dplyr::select(AA, Codon, Sr_optimal) %>%
     dplyr::mutate(Codon = tolower(Codon)) %>%
     group_by(AA) %>%
     dplyr::mutate (Sr_relAdapt = Sr_optimal / max(Sr_optimal))
 
-Ce_codonChart <- codon_usage_chart %>%
-    dplyr::select(-Sr_optimal) %>%
+Ce_codonChart <- Ce.codon.freq %>%
+    dplyr::select(AA, Codon, Ce_optimal) %>%
     dplyr::mutate(Codon = tolower(Codon)) %>%
     group_by(AA) %>%
     dplyr::mutate (Ce_relAdapt = Ce_optimal / max(Ce_optimal))
 
-codon_report <- dplyr::full_join(Sr_codonChart, 
-                                 Ce_codonChart,
-                                      by = c("AA", "Codon")
-)
+Bm_codonChart <- Bm.codon.freq %>%
+    dplyr::select(AA, Codon, Bm_optimal) %>%
+    dplyr::mutate(Codon = tolower(Codon)) %>%
+    group_by(AA) %>%
+    dplyr::mutate (Bm_relAdapt = Bm_optimal / max(Bm_optimal))
 
-write_csv(codon_report,
+rel_adaptiveness_chart <- dplyr::full_join(Sr_codonChart, Ce_codonChart,
+                                      by = c("AA", "Codon")) %>%
+    dplyr::full_join(Bm_codonChart, by = c("AA", "Codon")) %>%
+    dplyr::mutate(Codon = tolower(Codon))
+
+write_csv(rel_adaptiveness_chart,
           path = "../Outputs/rel_adaptiveness_chart.csv")
 
+# Generate Lookup Tables for each species ----
 
+## For each species, filter the relative adaptiveness chart
+## to generate a lookup table with the "optimal" codon
+
+### Strongyloides
+Sr_codonLut <- Sr_codonChart %>%
+    dplyr::filter(Sr_relAdapt == 1) %>%
+    dplyr::select(c(AA, Codon)) %>%
+    dplyr::rename(Codon.Sr = Codon) %>%
+    ungroup() 
+
+### C. elegans
+Ce_codonLut <- Ce_codonChart %>%
+    dplyr::filter(Ce_relAdapt == 1) %>%
+    dplyr::select(c(AA, Codon)) %>%
+    dplyr::rename(Codon.Ce = Codon) %>%
+    ungroup() 
+
+### B. malayi 
+Bm_codonLut <- Bm_codonChart %>%
+    dplyr::filter(Bm_relAdapt == 1) %>%
+    dplyr::select(c(AA, Codon)) %>%
+    dplyr::rename(Codon.Bm = Codon) %>%
+    ungroup() 
+
+### P. pacificus
+#### P. pacificus data already comes as a list of the optimal codons
+#### Load P. pacificus list of codons used in 3% highly expressed genes
+#### Source: Han *et al* 2020 https://www.genetics.org/content/216/4/947
+Pp_codonLut <- read_csv('../Data/Pp_optimal_codons.csv', 
+                          quote = "", 
+                          col_types = 'fc'
+)%>%
+    dplyr::mutate(Codon = tolower(Codon)) %>%
+    dplyr::rename(Codon.Pp = Codon)
+
+codon_lookup_tbl <- dplyr::full_join(Sr_codonLut, Ce_codonLut,
+                                      by = "AA") %>%
+    dplyr::left_join(Bm_codonLut, by = "AA") %>%
+    dplyr::left_join(Pp_codonLut, by = "AA")
+
+write_csv(codon_lookup_tbl,
+          path = "../Outputs/codon_lut.csv")
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Strongyloides Codon Adapter
-Preprocessing and analysis related to the *Strongyloides* Codon Adapter app, a web-based Shiny app for automatic codon optimzation and analysis based on *Strongyloides* codon usage.  
+# Wild Worm Codon Adapter Preprocessing & Analysis
+Preprocessing and analysis related to the Wild Worm Codon Adapter app, a web-based Shiny app for automatic codon optimzation and analysis based on codon usage rules in "wild" worm species, including: *Strongyloididae* species, *Pristionchus* species, *Brugia malayai*, as well as custom codon usage rules provided by users. 
 
 ## Table of Contents  
 1. [General Information](#general-information)
@@ -11,10 +11,14 @@ Preprocessing and analysis related to the *Strongyloides* Codon Adapter app, a w
 7. [Authors](#authors)
 
 ## General Information
-This repository contains the preprocessing scripts for generating necessary inputs for the *Strongloides* Codon Adapter app. It also contains analysis code used to generate results discussed in Bryant and Hallem (2021). 
+This repository contains the preprocessing scripts for generating necessary inputs for the Wild Worm Codon Adapter app. It also contains analysis code used to generate results discussed in Bryant and Hallem (2021). 
 
 ## App Preprocessing
-This subfolder contains preprocessing scripts for the *Strongylodies* Codon Adapter App. Specifically, the code calculats codon usage rules for both *Strongyloides* and *C. elegans* based on previously published codon count data. For *Strongyloides*, codon occurrence values are from highly expressed S. ratti transcripts [1]. For *C. elegans*, values are from genes with the highest bias toward translationally optimal codons [2].
+This subfolder contains preprocessing scripts for the Wild Worm Codon Adapter App. 
+
+First, the code calculates relative adaptation indeces for both *Strongyloides*, *C. elegans*, and *B. malayi* based on previously published codon count data. For *Strongyloides*, codon occurrence values are from highly expressed S. ratti transcripts [1]. For *C. elegans*, values are from genes with the highest bias toward translationally optimal codons [2]. Codon frequency rates for *B. malayi* are based on count data from the [*Brugia* Codon Usage Table](http://big.icp.ucl.ac.be/~opperd/private/C_U_B_%20Table.html). The relative adaptiveness values of every possible codon was generated as follows: individual codons were scored by calculating their relative adaptivness: (the frequency that codon "i" encodes amino acid "AA") / (the frequency of the codon most often used for encoding amino acid "AA"). 
+
+Next, optimal codon lookup tables are generated. For *S. ratti*, *C. elegans*, and *B. malayi*, optimal codons were defined as the codon with the highest relative adaptiveness value for each amino acid. For *Pristionchus* species, optimal codons were defined by codon usage bias calculations based on the top 10% highly expressed *P. pacificus* genes [5].
   
 ## Analysis Code  
 This subfolder contains  scripts (in an RMarkdown file) that generate analyses and plots included in Bryant and Hallem (2021). Specifically, these analyses seek to assess genome-wide codon bias patterns in *Strongyloides* species versus *C. elegans.* In brief, FASTA files containing all coding sequences (CDS) for *S. stercoralis*, *S. ratti*, *S. papillosus*, *S. venezuelensis*, and *C. elegans* were downloaded from Wormbase ParaSite (WBPS15) and analyzed using the *Strongyloides* Codon Adapter app. Results are located in the Data folder. Within the RMarkdown file, distributions of GC ratio and codon adaptation index values across species are plotted and statistically compared [3,4]. Then, results for each species are filtered to identify the following 6 functional subsets for each species: 2% with highest and lowest *Str*-CAI values, 2% with highest and lowest GC ratio, and 2% with highest and lowest *Ce*-CAI values. GO analyses of functional subsets are performed using the gprofiler2 package v0.1.9, with a false discovery rate (FDR)-corrected p-value < 0.05. Commonly enriched GO terms in each subset are defined as GO terms that were enriched in all *Strongyloides* species with an FDR-corrected p-value of < 0.001.
@@ -24,6 +28,7 @@ This subfolder contains  scripts (in an RMarkdown file) that generate analyses a
 2. [Sharp and Bradnam (1997). Appendix 3: Codon Usage in *C. elegans*. In: *C. elegans* II. 2nd edition; Eds: Riddle, Blumenthal, Meyer *et al*. Cold Spring Harbor Laboratory Press.](https://www.ncbi.nlm.nih.gov/books/NBK20194/).
 3. [Sharp and Li (1987). The Codon Adaptation Index: a measure of directional synonymous codon usage bias, and its potential applications. *Nucleic Acids Research* 15: 1281-95](https://pubmed.ncbi.nlm.nih.gov/3547335/). 
 4. [Jansen *et al* (2003). Revisiting the codon adaptation index from a whole-genome perspective: analyzing the relationship between gene expression and codon occurrence in yeast using a variety of models. *Nucleic Acids Research* 31: 2242-51](http://www.ncbi.nlm.nih.gov/pubmed/12682375). 
+5. [(Han *et al*, 2020)](https://www.genetics.org/content/216/4/947).
 
 ## Sources  
 * [Shiny](https://shiny.rstudio.com/) - UI framework
@@ -31,8 +36,10 @@ This subfolder contains  scripts (in an RMarkdown file) that generate analyses a
 * [Seqinr](https://www.rdocumentation.org/packages/seqinr/versions/3.6-1) - Utilities for calculating Codon Adaptation Index
 * Codon Usage Patterns:  
   - *Strongyloides spp*: [Mitreva *et al* 2006](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1779591/)
+  - *Pristionchus spp*: [Han *et al* (2020)](https://www.genetics.org/content/216/4/947)
+  - *Brugia malayi*: [*Brugia* Codon Usage Table](http://big.icp.ucl.ac.be/~opperd/private/C_U_B_%20Table.html)
   - *C. elegans*: [Sharp and Bradnam, 1997](https://www.ncbi.nlm.nih.gov/books/NBK20194/)
-
+  
 ## License  
 This project is licensed under the MIT License. 
 
